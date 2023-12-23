09d48e1a2b014975d0a04f5e8a79e7d4d2bd045e
ashwinikalantri <ak@ashwini.co.in>
2023-12-23 12:03:15 +0530

    adding code to the anthro script

--- scripts/anthro.R
+++ scripts/anthro.R
@@ -1,2 +1,36 @@
 library(anthro)
+library(readr)
+library(eeptools)
+library(ggplot2)
+
+setwd("~/r4ph23")
+
+rm(list = ls())
+
+anthro <- read_csv("data/anthro.csv")
+
+anthout <- anthro %>%
+  mutate(age = age_calc(dob, visit_date, units = "months")) %>%
+  mutate(anthro_zscores(
+    sex = sex,
+    age = age,
+    is_age_in_month = T,
+    weight = wt,
+    lenhei = ht,
+    measure = "h"
+  ))
+
+anthro_zscores(sex = "M",
+               age = 450,
+               is_age_in_month = F,
+               weight = 12,
+               lenhei = 80,
+               measure = "l")
+
+anthout %>% 
+  ggplot() +
+  geom_density(aes(x = zwei)) +
+  geom_function(fun = dnorm, 
+                args = list(mean = 0,
+                            sd = 1))
 
